5e7def39677098b15458f64f8e76c791f90a8801
Sebastien Perez <sebastien.perez@amadeus.com>
2015-01-26 12:56:54 +0100

    "First commit based on stackoverflow"

--- TI.js
+++ TI.js
@@ -1,18 +1,27 @@
-(function(window) {
+'use strict';
 
-    'use strict';
+(function(module) {
+    if (typeof define === "function" && define.amd) {
+        define(function() {
+            return module;
+        });
+    } else {
+        window.myfunction = module.myfunction;
+    }
+}({
+    define_TILibrary: function() {
 
-    function define_TILibrary() {
         var TILibrary = {};
 
+        TILibrary.version = "0.1"
         TILibrary.token = ""
-	TILibrary.backend = "https://beta-api.travel-intelligence.com/"
+        TILibrary.backend = "https://beta-api.travel-intelligence.com/"
 
-	TILibrary.configure = function(backend) {
-	    TILibrary.backend = backend;
+        TILibrary.configure = function(backend) {
+            TILibrary.backend = backend;
         }
 
-	TILibrary.login = function(email, pass, callback) {
+        TILibrary.login = function(email, pass, callback) {
             $.ajax({
                 type: "POST",
                 url: TILibrary.backend + "api/v1/session",
@@ -24,14 +33,14 @@
                 }
             }).done(function(msg) {
                 var token = msg.session.auth_token
-		TILibrary.token = token
+                TILibrary.token = token
                 callback(msg)
             })
         }
 
         //Logout
         TILibrary.logout = function(callback) {
-	    
+
             //Logged out
             $.ajax({
                 type: "DELETE",
@@ -64,12 +73,4 @@
 
         return TILibrary;
     }
-
-    //define globally if it doesn't already exist
-    if (typeof(TI) === 'undefined') {
-        window.TI = define_TILibrary();
-    } else {
-        console.log("TI already defined.");
-    }
-
-})(window);
+}));
